00fec9496d48eb2bb86d80681ac91540cbce1ec9
vbacic <valentina.bacic2@gmail.com>
2021-05-30 14:55:11 +0200

    dohvat lokacije u tablici

--- src/Components/UpravljanjeDojavama.jsx
+++ src/Components/UpravljanjeDojavama.jsx
@@ -1,7 +1,10 @@
-import React, { Component } from 'react';
-import { Button, ButtonGroup, Container, Table } from 'reactstrap';
+import { Component } from 'react';
+import React from 'react';
+import {Table} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import {db} from '../firebase';
+import '../Style.css';
+
 
 class ListaDojava extends Component {
   constructor(props) {
@@ -28,7 +31,7 @@ class ListaDojava extends Component {
    onCollectionUpdate = (querySnapshot) => {
      const dojave = [];
      querySnapshot.forEach((doc) => {
-       const { ime_k,prezime_k,broj_mobitela,vrsta_nesrece,broj_ozljedenih,datum,hitnoca_nesrece } = doc.data();
+       const { ime_k,prezime_k,broj_mobitela,vrsta_nesrece,broj_ozljedenih,datum,hitnoca_nesrece, geo } = doc.data();
        dojave.push({
          key: doc.id,
          doc, // DocumentSnapshot
@@ -38,7 +41,8 @@ class ListaDojava extends Component {
          vrsta_nesrece,
          hitnoca_nesrece,
          broj_ozljedenih,
-         datum
+         datum,
+         geo
        });
      });
      this.setState({
@@ -48,9 +52,9 @@ class ListaDojava extends Component {
   render() {
     return (
       
-      <div>
+      <div ClassName="table">
         
-            <table>
+            <Table>
               <thead>
                 <tr>
                   <th>Ime</th>
@@ -59,7 +63,9 @@ class ListaDojava extends Component {
                   <th>Broj ozljeđenih</th>
                   <th>Hitnoća</th>
                   <th>Vrsta nesrece</th>
-                  <th>Datum</th>
+                  <th>Datum </th>
+                  <th>Lokacija</th>
+                  <th>Akcije</th>
                 </tr>
               </thead>
               <tbody>
@@ -70,14 +76,19 @@ class ListaDojava extends Component {
                     <td>{dojave.broj_mobitela}</td>
                     <td>{dojave.broj_ozljedenih}</td>
                     <td>{dojave.hitnoca_nesrece}</td>
-                    <td>{dojave.vrsta_nesrece}</td>
+                    <td>{dojave.vrsta_nesrece} </td>
                     <td>{dojave.datum}</td>
-                    <Link to={`/edit/${dojave.key}`}>Pregledaj i uredi</Link>;
-                    <button onClick={this.delete.bind(this, dojave.key)}>Izbrisi</button>
+                    <td>geografska dužina: {dojave.geo.longitude}, 
+                    geografska širina: {dojave.geo.latitude}</td>
+                   
+                    <Link to={`/edit/${dojave.key}`}>  <button className="block  mx-auto text-white rounded bg-orange-400 border border-b-4 border-orange-600" >
+    Uredi
+  </button></Link> 
+                    <button className="block  mx-auto text-white rounded bg-green-400 border border-b-4 border-green-600" onClick={this.delete.bind(this, dojave.key)}>Izbrisi</button>
                   </tr>
                 )}
               </tbody>
-            </table>
+            </Table>
             </div>
     );
   }
