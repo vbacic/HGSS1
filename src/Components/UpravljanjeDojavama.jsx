3279cf20f3287cfe9be5ba22de366f6c8181de0c
vbacic <valentina.bacic2@gmail.com>
2021-05-31 12:31:51 +0200

    brisanje importa za Link

--- src/Components/UpravljanjeDojavama.jsx
+++ src/Components/UpravljanjeDojavama.jsx
@@ -1,7 +1,6 @@
 import { Component } from 'react';
 import React from 'react';
 import {Table} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import {db} from '../firebase';
 import '../Style.css';
 
