f233ec78e1ca3786d554e085ba4676fac47d2432
vbacic <valentina.bacic2@gmail.com>
2021-05-31 12:29:24 +0200

    izmjena home stranice

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 import UnosDojave from './UnosDojave';
+
 const Home = () => {
   return (
-    <div className="container md:w-3/4 mx-auto mt-20 mb-10">
-
-
-      <p className="w-11/12 lg:w-3/4 mx-auto text-3xl mb-10">
-        
-      </p>
+    <div>
       <UnosDojave />
-      <p className="w-11/12 lg:w-3/4 text-3xl mx-auto">
-       
-      </p>
     </div>
   );
 };
